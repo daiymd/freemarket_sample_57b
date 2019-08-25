d8971832da90487acd27745700eaf5753ca0452c
hibiki0293 <hibiki893@i.softbank.jp>
2019-08-25 14:20:33 +0900

    エラー回避

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :products, through: :transactions
-  has_many :transactions
-  has_many :comments
-  has_many :likes
-  has_one :street_adress
-  has_many :buyers
-  has_many :sellers
-  has_one :payment
-  has_many :evaluations       
+  # has_many :products, through: :transactions
+  # has_many :transactions
+  # has_many :comments
+  # has_many :likes
+  # has_one :street_adress
+  # has_many :buyers
+  # has_many :sellers
+  # has_one :payment
+  # has_many :evaluations       
 end
