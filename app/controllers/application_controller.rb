7e1715e465d86d5903cdddf01b0f87290718e46f
hibiki0293 <hibiki893@i.softbank.jp>
2019-08-26 17:24:45 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '57_mercari_b' && password == '57b'
+    end
+  end
 end
