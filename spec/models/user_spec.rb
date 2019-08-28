d477c14d990aa491cd6ad0ce5f5bcc37535fcd44
dai <dai.poke@icloud.com>
2019-08-28 22:49:03 +0900

    Correction
    ・hamlに変換した。
    ・validationで弾かれる条件(空白)では登録できないことを、テストで確認した。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,53 @@ describe User do
       user = FactoryBot.build(:user)
       expect(user).to be_valid
     end
+
+    it "is invalid without a name" do
+      user = FactoryBot.build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid without a nickname" do
+      user = FactoryBot.build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "is invalid without a furigana" do
+      user = FactoryBot.build(:user, furigana: nil)
+      user.valid?
+      expect(user.errors[:furigana]).to include("can't be blank")
+    end
+
+    it "is invalid without a email" do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "is invalid without a password" do
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "is invalid without a password_confirmation" do
+      user = FactoryBot.build(:user, password_confirmation: nil)
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("can't be blank")
+    end
+
+    it "is invalid without a birthday" do
+      user = FactoryBot.build(:user, birthday: nil)
+      user.valid?
+      expect(user.errors[:birthday]).to include("can't be blank")
+    end
+
+    it "is invalid without a tell" do
+      user = FactoryBot.build(:user, tell: nil)
+      user.valid?
+      expect(user.errors[:tell]).to include("can't be blank")
+    end
   end
 end
