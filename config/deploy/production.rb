6b466f0f98bdb51857b07dd7217d2024ec4ba33d
hibiki0293 <hibiki893@i.softbank.jp>
2019-08-28 13:31:35 +0900

    basic認証の導入

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '13.112.231.24', user: 'ec2-user', roles: %w{app db web}
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
