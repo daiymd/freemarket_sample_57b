227475569dd2a172a5f2815a09e7e04c845450d4
dai <dai.poke@icloud.com>
2019-09-03 16:36:58 +0900

    sns_credential
    facebookとgoogleのSNS認証を実装した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_062126) do
+ActiveRecord::Schema.define(version: 2019_09_03_052906) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_062126) do
     t.string "provider", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "street_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
