f1169e32c14546f2ca1bd21b2fef5b3b308b9561
oda <chunyex@gmail.com>
2019-09-03 12:15:17 +0900

    set routing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,12 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def show
+    render "products/#{params[:name]}"
+  end
+
+  def new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root "users#show"
   get 'users/:name', controller: 'users', action: 'edit'
+  get 'products/:name', controller: 'products', action: 'show'
   resources :users, only: [:show] 
   resources :products
 end
