cc056f4058a1929739b9a1368977ed3ca442701b
hibiki0293 <hibiki893@i.softbank.jp>
2019-09-04 16:12:43 +0900

    画像投稿

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,26 @@ class ProductsController < ApplicationController
   end
 
   def new
-    
+    @product = Product.new
+    @product.images.build
   end
-  
+  def create
+    @product = Product.new(product_parameter)
+    respond_to do |format|
+      if @product.save
+          params[:images][:image].each do |image|
+            @product.images.create(image: image, product_id: @product.id)
+          end
+        format.html{redirect_to root_path}
+      else
+        @product.images.build
+        format.html{render action: 'new'}
+      end
+    end
+  end
+
+  def product_parameter
+    params.require(:product).permit(:name, :price, :status, :delivery_price, :delivery_way, :scheduled, images_attributes: [:image]).merge(user_id: current_user.id)
+  end
+
 end
