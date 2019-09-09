09bd4febec8d0ecd9a796e0f843c41c92faa6a42
oda <chunyex@gmail.com>
2019-09-09 14:35:47 +0900

    ハッシュロケットの修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -16,9 +16,9 @@ class PurchaseController < ApplicationController
     card = Payment.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => 61500, 
-    :customer => card.customer_id, 
-    :currency => 'jpy', 
+    amount: 61500, 
+    customer: card.customer_id, 
+    currency: 'jpy', 
   )
   redirect_to action: 'done' 
   end
